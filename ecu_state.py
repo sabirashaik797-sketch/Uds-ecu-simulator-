93845775d86fc5b07b13feda1f21693d633c17b0
sabirashaik797-sketch <sabirashaik797@gmail.com>
2026-01-09 12:04:23 +0530

    Add ECU State class for internal state management
    Initial implementation of ECUState class to manage session and security status.

---
+++ ecu_state.py
@@ -0,0 +1,9 @@
+"""
+ECU internal state (memory)
+Keeps track of session and security status
+"""
+
+class ECUState:
+    def __init__(self):
+        self.session = 0x01          # Default Session
+        self.security_unlocked = False
